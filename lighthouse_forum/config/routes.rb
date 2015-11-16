34642d81df7ce610a7d4231ac9f20a19b4e1a659
Jas Der <jasmineder@gmail.com>
2015-11-16 20:38:43 +0000

    step 10

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -1,10 +1,15 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
+  resources :posts
 
-  get 'posts/:id', to:'posts#show'
+  # get 'posts', to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id', to:'posts#show', as: 'post'
+  # patch 'posts/:id', to:'posts#update'
+  # post 'posts', to: 'posts#create'
 
-
+end
 
 
 
@@ -63,4 +68,3 @@ LighthouseForum::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
