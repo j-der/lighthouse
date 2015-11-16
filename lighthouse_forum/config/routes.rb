18f685f20ba3f3b9aca0924daec7e21b96b711a7
Jas Der <jasmineder@gmail.com>
2015-11-16 03:20:51 +0000

    step 2

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -1,4 +1,14 @@
 LighthouseForum::Application.routes.draw do
+
+  get 'posts', to: 'posts#index'
+
+  # [helpful comments]
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +49,7 @@ LighthouseForum::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
